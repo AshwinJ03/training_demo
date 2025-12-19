3ec9d7c3b5c124d472d4eaafb635967dee4e0634
Ashwin09072003 <ashwinjai07@gmail.com>
2025-12-19 14:49:11 +0530

    added hello

--- cookie_proj/app/urls.py
+++ cookie_proj/app/urls.py
@@ -24,3 +24,10 @@ urlpatterns = [
     path('logout/', logout_view, name='logout'),
 ]
 from .views import LoginTokenAPI"""
+
+urlpatterns = [
+    path('', login_view, name='login'),
+    path('hello/', profile, name='profile'),
+    path('about/', about, name='about'),
+    path('logout/', logout_view, name='logout'),
+]
